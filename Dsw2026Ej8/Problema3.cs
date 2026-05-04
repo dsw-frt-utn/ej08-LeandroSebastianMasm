dd1b1f4a1dbe27b75e7e90e7e8861db7d5d674d2
Leandro Sebastian Masmut <Leandro.Masmut@alu.frt.utn.edu.ar>
2026-05-04 14:08:33 -0300

    Problema 3

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int CopyValue = originalValue;
+            CopyValue++;
+            Product productCopy = product;
+            productCopy.Description = "Modificado";
+            return $"{originalValue}-{CopyValue}-{product.Description}";
+        }
+    }
+}
