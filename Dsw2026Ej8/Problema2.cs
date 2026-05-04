f5a93e87b375450010ae087cd9c68a0b405acd66
Leandro Sebastian Masmut <Leandro.Masmut@alu.frt.utn.edu.ar>
2026-05-04 13:52:34 -0300

    Problema 2

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal total = 0;
+            if (quantity > 0)
+            {
+                total = quantity * unitPrice;
+            }
+            else
+            {
+                total = 0;
+            }
+
+            var resumenVenta = new
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total = total
+            };
+            return $"{resumenVenta.Code}-{resumenVenta.Description}-{resumenVenta.Total:C}";
+        }
+    }
+}
+
