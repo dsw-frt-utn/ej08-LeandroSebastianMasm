1303ff2ee808cf2da3eea97d46a52a688a5c8ea8
Leandro Sebastian Masmut <Leandro.Masmut@alu.frt.utn.edu.ar>
2026-05-04 14:35:56 -0300

    Problema 4

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            double suma = 0;
+            double Promedio;
+            List<int> notasValidas = new List<int>();
+            int?[] notas = { nota1, nota2, nota3 };
+
+            foreach (int? nota in notas)
+            {
+                if (nota != null && nota.Value >= 0 && nota.Value <= 10)
+                {
+                    notasValidas.Add(nota.Value);
+                }
+            }
+            if (notasValidas.Count == 0)
+            {
+                return 0;
+            }
+
+            foreach (int nota in notasValidas)
+            {
+                suma += nota;
+            }
+            Promedio = suma / notasValidas.Count;
+            return Promedio;
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -8,6 +8,10 @@
             string etiqueta = helper.ObtenerEtiquetaProducto(12345, "Laptop Gamer", 1200.50m);
 
             Console.WriteLine(etiqueta);
+
+            Problema4 Prom = new Problema4 ();
+            double ABC = Prom.CalcularPromedio(null, 1, 10);
+            Console.WriteLine(ABC);
         }
     }
 }
