a703ed10eaa72d44ff2458f1007c402a5aad0505
Leandro Sebastian Masmut <Leandro.Masmut@alu.frt.utn.edu.ar>
2026-05-04 15:33:29 -0300

    Problema 5

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -12,6 +12,16 @@
             Problema4 Prom = new Problema4 ();
             double ABC = Prom.CalcularPromedio(null, 1, 10);
             Console.WriteLine(ABC);
+            //-----------------------------------------------------------------------------
+            var prob5 = new Problema5();
+
+            RetailSale ventaMinorista = new RetailSale();
+            ventaMinorista.Importe= 100;
+            WholeSale ventaMayorista = new WholeSale();
+            ventaMayorista.Importe = 100;
+
+            Console.WriteLine(prob5.ObtenerImporteFinal(ventaMinorista));
+            Console.WriteLine(prob5.ObtenerImporteFinal(ventaMayorista)); 
         }
     }
 }
---
+++ Dsw2026Ej8/Sale.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Sale
+    {
+        public decimal Importe { get; set; }
+        public virtual decimal CalculateTotal()
+        {
+            return Importe;
+        }
+    }
+
+    public class RetailSale : Sale
+    {
+        public override decimal CalculateTotal()
+        {
+            return Importe;
+        }
+    }
+
+    public class WholeSale : Sale
+    {
+        public override decimal CalculateTotal()
+        {
+            return Importe * (decimal)0.9;
+        }
+    }
+}
